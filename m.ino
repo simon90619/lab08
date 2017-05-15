37f095a22e42f5a8e09903fdc15f28a845b6948f
simon90619 <simon1996524@gmail.com>
2017-05-15 18:47:33 +0800

    Add files via upload

---
+++ m.ino
@@ -0,0 +1,35 @@
+#include <LiquidCrystal.h>
+#include <Wire.h>
+const int SLAVE_ADDRESS = 1;
+char incomingByte = 0;
+LiquidCrystal lcd(12, 7, 5, 4, 3, 2);
+
+void setup() 
+{
+  Wire.begin();
+  Serial.begin(9600);
+  lcd.begin(20, 2);
+  lcd.cursor();
+Serial.println("Humidity and temperature\n\n");
+}
+
+void loop() 
+{     
+  char c;
+  Wire.requestFrom(8,12); 
+   while (Wire.available())
+   {    
+        lcd.clear();
+         while(Wire.available())
+        {
+         delay(100);
+            
+            c = Wire.read(); 
+            lcd.write(c); 
+        }
+        
+   }
+        delay(100);
+   Serial.println("");
+ }
+
